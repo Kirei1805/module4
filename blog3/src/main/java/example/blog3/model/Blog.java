d044fde9429b1e8ef7a5febe50d978960ddd0e8c
Kirei1805 <loiphan1805@gmail.com>
2025-10-20 16:48:56 +0700

    nop bai

--- blog3/src/main/java/example/blog3/model/Blog.java
+++ blog3/src/main/java/example/blog3/model/Blog.java
@@ -23,7 +23,6 @@ public class Blog {
         this.createdAt = LocalDateTime.now();
     }
 
-    // Getter & Setter
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
     public String getTitle() { return title; }
