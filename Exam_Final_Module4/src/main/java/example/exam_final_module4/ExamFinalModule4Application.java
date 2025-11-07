1304a566274a06995323524235cc6f7902af48bc
Kirei1805 <loiphan1805@gmail.com>
2025-11-07 17:49:56 +0700

    test nop file

---
+++ Exam_Final_Module4/src/main/java/example/exam_final_module4/ExamFinalModule4Application.java
@@ -0,0 +1,13 @@
+package example.exam_final_module4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamFinalModule4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamFinalModule4Application.class, args);
+    }
+
+}
---
+++ Exam_Final_Module4/src/test/java/example/exam_final_module4/ExamFinalModule4ApplicationTests.java
@@ -0,0 +1,13 @@
+package example.exam_final_module4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamFinalModule4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
